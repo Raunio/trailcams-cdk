9355bc9fbe96778d5ddaff2a9c58645f0fb9e0fd
Niko Tiikkaja <niko.tiikkaja@gmail.com>
2023-02-04 12:17:34 +0200

    Initial commit

---
+++ test/trailcams-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrailcamsCdk from '../lib/trailcams-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trailcams-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrailcamsCdk.TrailcamsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
